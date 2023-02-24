5cbfc8445ae62f596998f8e0becce2dd9eaca1cc
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-02-24 22:22:35 +0530

    projects

--- JAVA_DSA/src/Main.java
+++ JAVA_DSA/src/Main.java
@@ -1,5 +1,9 @@
+import non_primitive_datastructers.PrimitiveDataStructers;
+
 public class Main {
     public static void main(String[] args) {
+        PrimitiveDataStructers primitiveDataStructers = new PrimitiveDataStructers();
+        primitiveDataStructers.addTwoNumbers(23);
 
         System.out.println("Hello world!");
     }
---
+++ JAVA_DSA/src/non_primitive_datastructers/PrimitiveDataStructers.java
@@ -0,0 +1,9 @@
+package non_primitive_datastructers;
+
+public class PrimitiveDataStructers {
+  public int addTwoNumbers(int x){
+            System.out.println("hello this is yogesh joga");
+      return 0;
+  }
+
+}
